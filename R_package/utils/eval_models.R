1f1adc4653f73679e4504dff76f7a47a1ab62f09
Jeferson Rodriguez <jeferson_88@hotmail.com>
2024-03-05 20:17:03 -0500

    Update eval_models.R
    text_generic function to extract

--- R_package/utils/eval_models.R
+++ R_package/utils/eval_models.R
@@ -17,83 +17,87 @@
 
 import_exp_data <- function(path_data, INPUT_data_files, cultivar, model = "oryza"){
   
-  # listar los archivos o listas de experimentos disponibles en la carpeta de datos
-  
-  # files_cultivar <- list.files(path, pattern = fixed(cultivar))
-  
-  ## Importa datos de trabajo
-  data <- INPUT_data_files %>% 
+# Import and process data files
+  data <- files %>%
     enframe(name = NULL, value = "file") %>%
-    mutate(loc_cul = str_sub(file, 1,-6)) %>% 
-    separate(col = loc_cul, into = c("site", "cultivar"), sep = "_") %>%
-    mutate(input_data =  map(file, ~read_INPUT_data(paste0(path_data, .))))
-  
-  ## Extrae datos de suelo
-  soil <- data %>% mutate(soil_data = map(input_data, ~.x$SOIL_obs))  %>% dplyr::select(-input_data)  %>% 
-    unnest(soil_data) %>% 
-    mutate(LOC_ID = str_sub(ID, 1, 4)) %>% group_by(LOC_ID, NL) %>%
-    summarize_if(is.numeric, .funs = mean_boot) %>%
-    mutate(ID=LOC_ID, STC=get_STC(SAND, CLAY)) %>% ungroup() %>%  
-    mutate(SOC = SC) %>% split(.$ID) %>% 
-    enframe(name = "site", value = "soil")
-  
-  
+ #   mutate(loc_cul = str_sub(file, 1,-6)) %>%
+  #  separate(col = loc_cul, into = c("site", "cultivar"), sep = "_") %>%
+    mutate(input_data =  map(file, ~read_agroclimr_data(paste0(path, .))))
+
+
+  ## Extrae location data
+  location_data <- data$input_data %>% map("AGRO_man") %>% bind_rows() %>%
+    dplyr::select(LOC_ID, LAT, LONG, ALT) %>% #contains("LAT"), contains("LONG"), starts_with("A")) %>%
+    distinct() %>% set_names(c("site", "lat", "lon", "elev")) %>%
+    group_by(site) %>% slice(1) %>%
+    ungroup()
+
+
+  # Extract soil data
+
+  soil_data <- data %>%
+    mutate(soil_data = map(input_data, ~.x$SOIL_obs))  %>%
+    dplyr::select(-input_data)  %>%
+    unnest(soil_data) %>%
+   # mutate(LOC_ID = str_sub(ID, 1, 4)) %>%
+    group_by(LOC_ID, NL) %>%
+    summarize_if(is.numeric, .funs = mean) %>%
+    mutate(ID=LOC_ID, STC = get_STC(SAND, CLAY)) %>% ungroup() %>%
+    split(.$ID) %>%
+    enframe(name = "site", value = "soil") %>%
+    left_join(location_data, by = "site")
+
+
   ## Extrae datos de clima
-  
-  wth <- data %>% mutate(ws_id = 1, wth = map(input_data, ~.x$WTH_obs )) %>% 
-    select(site, ws_id, wth) %>% 
-    #  group_by(localidad) %>% slice(1) %>% 
-    unnest(wth) %>% 
+
+  wth_data <- data %>% mutate(wth = map(input_data, ~.x$WTH_obs )) %>%
+    select(file, wth) %>%
+    #  group_by(localidad) %>% slice(1) %>%
+    unnest(wth) %>%
     mutate(DATE = as.Date(DATE)) %>%
     #  mutate(wspd = suppressWarnings(as.numeric(WVEL))) %>% ###### si existen ?
-    set_names(tolower(names(.))) %>% 
-    #  dplyr::select(-wvel) %>%
-    dplyr::distinct() %>% 
-    nest(wth = -c(site, ws_id)) %>% 
-    left_join(
-      
-      data$input_data %>% map("AGRO_man") %>% bind_rows() %>% 
-        dplyr::select(LOC_ID, contains("LAT"), contains("LONG"), starts_with("A")) %>% 
-        distinct() %>% set_names(c("site", "lat", "lon", "elev")) %>%
-        group_by(site) %>% slice(1) %>% 
-        ungroup(),
-      by = "site") %>% rename(stn = ws_id) #%>% 
+    set_names(tolower(names(.))) %>%
+    dplyr::select(-file) %>%
+    dplyr::distinct() %>%
+    nest(wth = -contains("id")) %>% rename(site = loc_id ) %>%
+    left_join(location_data, by = "site") #%>% rename(stn = ws_id) #%>%
   #  mutate(path = "data/OUTPUTS/WTH/") %>%
   #  select(path, id_name, wth_data, lat, lon, elev, stn)
-  
+
   # Extract and plot - Growth and Development observed data by component
-  
+
   # Crop Phenology
-  phen <- extract_obs_var(data$input_data, "phen")
+  phen <- extract_obs_var(data$input_data, "phen", model)
   #  plot_phen_obs(phen) %>% ggplotly()
-  
+
   #Leaf Area Index
-  lai <- extract_obs_var(data$input_data, "lai")
-  #  plot_lai_obs(lai) %>% ggplotly() 
-  
+  lai <- extract_obs_var(data$input_data, "lai", model)
+  #  plot_lai_obs(lai) %>% ggplotly()
+
   #Shoot Dry Matter
-  dry_matter <- extract_obs_var(data$input_data, "dry_matter")
+  dry_matter <- extract_obs_var(data$input_data, "dry_matter", model)
   #  plot_drymatter_obs(dry_matter) %>% ggplotly()
-  
+
   #Yield dry matter
-  yield <- extract_obs_var(data$input_data, "yield")
+  yield <- extract_obs_var(data$input_data, "yield", model)
   #  plot_yield_obs(yield) %>% ggplotly()
-  
-  
-  
+
+
+
   # crea lista de datos extraidos
   data_list <- list(
-    
-    data =  data %>% left_join(soil, by = "site") %>% left_join(wth, by = "site"),
-    
+
+    data =  data , soil = soil_data, wth = wth_data,
+
     phen =  phen, lai = lai, dry_matter = dry_matter, yield = yield
-    
+
   )
-  
-  
-  
-  
+
+
+
+
   return(data_list)
+
   
   
 }
